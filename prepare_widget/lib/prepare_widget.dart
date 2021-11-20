7018210ad8323bed617590e3fd476c0bcb3a30a2
miliapus <992450792@qq.com>
2021-11-20 19:12:34 +0800

    add prepare widget

--- prepare_widget/lib/prepare_widget.dart
+++ prepare_widget/lib/prepare_widget.dart
@@ -1,7 +1,34 @@
-library prepare_widget;
+import 'dart:async';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/widgets.dart';
+
+class PrepareWidget extends StatefulWidget {
+  final FutureOr<Widget> Function(BuildContext context) builder;
+  final Widget loading;
+
+  const PrepareWidget({
+    Key? key,
+    required this.builder,
+    required this.loading,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => PrepareWidgetState();
+}
+
+class PrepareWidgetState extends State<PrepareWidget> {
+  Widget? child;
+
+  @override
+  void initState() {
+    super.initState();
+    () async {
+      child = widget.loading;
+      child = await widget.builder(context);
+      setState(() {});
+    }();
+  }
+
+  @override
+  Widget build(BuildContext context) => child!;
 }
